86478f418fa74276a21f5829f257df01772dcad0
taiganog <taiga.nog.b@gmail.com>
2025-05-22 19:09:50 -0300

    chore: adição de template das páginas de funções principais

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { Github, LayoutGrid, ArrowUpFromLine, Search } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -14,18 +14,23 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Upload',
+        href: '/upload',
+        icon: ArrowUpFromLine,
+    },
+    {
+        title: 'Search',
+        href: '/search',
+        icon: Search,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
+        title: 'Repo',
+        href: 'https://github.com/taiganog/docfetch-php',
+        icon: Github,
     },
 ];
 </script>
